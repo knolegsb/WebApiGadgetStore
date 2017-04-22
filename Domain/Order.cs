ef3f01efd9ddca5fe407f2efb7f4a20fb2b1214d
Sean <seon2you@hotmail.com>
2017-04-22 00:16:58 -0700

    Add project files.

---
+++ Domain/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Order
+    {
+        public Order()
+        {
+            Gadgets = new List<Gadget>();
+        }
+
+        public int OrderId { get; set; }
+        public string CompanyName { get; set; }
+        public string OwnerName { get; set; }
+        public string Street { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public List<Gadget> Gadgets { get; set; }
+    }
+}
